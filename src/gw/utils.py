196e48efe708255689cd6b10068ce97c0c271c6c
AtsushiMiyashita317 <miyashita.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2023-07-08 14:50:30 +0900

    zero interpolate

--- src/gw/utils.py
+++ src/gw/utils.py
@@ -38,17 +38,18 @@ def interpolate(x:torch.Tensor, ilens:torch.Tensor, olens:torch.Tensor, mode='li
         torch.Tensor: (B, T_o, F)
     """
     x = torch.nn.functional.pad(x, (0,0,0,2))
+    max_ilen = ilens.max().item()
     max_olen = olens.max().item()
     # (B, 1)
-    scales = ((ilens-1)/(olens-1))[:,None]
-    # (B, 1)
     batch = torch.arange(x.size(0),device=x.device)[:,None]
-    # (1, T_o)
-    otime = torch.arange(max_olen,device=x.device)[None,:]
-    # (B, T_o)
-    idx = scales.mul(otime)
-    idx[otime>=olens[:,None]] = -2
     if mode == 'linear':
+        # (B, 1)
+        scales = ((ilens-1)/(olens-1))[:,None]
+        # (1, T_o)
+        otime = torch.arange(max_olen,device=x.device)[None,:]
+        # (B, T_o)
+        idx = scales.mul(otime)
+        idx[otime>=olens[:,None]] = -2
         # (B, T_o)
         idx0 = idx.floor().long()
         # (B, T_o)
@@ -59,6 +60,25 @@ def interpolate(x:torch.Tensor, ilens:torch.Tensor, olens:torch.Tensor, mode='li
         y = (1-t)*x[batch,idx0] + t*x[batch,idx1]
         return y
     elif mode == 'nearest':
+        # (B, 1)
+        scales = ((ilens-1)/(olens-1))[:,None]
+        # (1, T_o)
+        otime = torch.arange(max_olen,device=x.device)[None,:]
+        # (B, T_o)
+        idx = scales.mul(otime)
+        idx[otime>=olens[:,None]] = -2
         idx0 = idx.round().long()
         y = x[batch, idx0]
         return y
+    elif mode == 'zero':
+        # (B, 1)
+        scales = ((olens-1)/(ilens-1))[:,None]
+        # (1, T_i)
+        itime = torch.arange(max_ilen,device=x.device)[None,:]
+        # (B, T_i)
+        idx = scales.mul(itime)
+        idx[itime>=ilens[:,None]] = -2
+        idx0 = idx.floor().long()
+        y = x.new_zeros((x.size(0), max_olen+2, x.size(2)))
+        y[batch, idx0] = x[:,:-2]
+        return y[:,:-2]
